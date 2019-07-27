f3295fe09f8b33693bc14beefdacff3bf294c271
Cristobal Fresno <cfresno@jedha.local>
2019-07-27 18:04:25 -0500

    Check requirements for libraries

---
+++ R/requirements.R
@@ -0,0 +1,48 @@
+#############################################################
+## R scripts requirements
+#############################################################
+## IDEA: Test if the required packages are installed and 
+##       install the missing packages.
+## Final: The script should end without 
+#############################################################
+##Required packages  
+rPackages <- c(
+  "tidyverse",
+  "optparse"
+)
+bioconductorPackages <- c(
+  "BiocParallel"
+)
+
+##Test packages
+testPackages <- function(x){
+  result <- lapply(x, require, character.only = TRUE)
+  result <- unlist(result)
+  return(result)
+}
+
+#Checking Packages
+resultR <- testPackage(rPackages)
+resultBioconductor <- testPackage(bioconductorPackages)
+
+##Install R Packages
+if(any(resultR == FALSE)){
+  out <- lapply(
+    packages[resultR == FALSE], 
+    install.packages
+  )
+  resultR <- testPackage(packages)
+}
+stopifnot(resultR)
+
+##Install Bioconductor Packages
+if(any(resultBioconductor == FALSE)){
+  out <- lapply(
+    bioconductorPackages[resultBioconductor == FALSE], 
+    BiocManager::install
+  )
+  resultBioconductor <- testPackage(bioconductorPackages)
+}
+
+stopifnot(resultBioconductor)
+#############################################################
