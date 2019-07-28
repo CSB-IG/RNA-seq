5657b84f50741e9564260c8e48bee215eb8f7fcc
SergioAndoengui <sergenesis@gmail.com>
2019-07-28 14:16:10 -0500

    attributes object and annot object with annotations, added

--- R/AnnotateData.R
+++ R/AnnotateData.R
@@ -7,11 +7,28 @@ source("./requirements.R")
 option_list = list(
   make_option(
     c("-b", "--biomaRt"),  
-    type = "integer", 
-    default = 94, 
-    help = "Ensembl version", 
+    type = "character", 
+    default = NULL, 
+    help = "Ensembl host", 
     metavar = "integer"
   )
 )
 opt_parser = OptionParser(option_list = option_list)
 opt = parse_args(opt_parser)
+
+ensembl <- useMart(biomart = "ensembl",
+                   host = opt$biomaRt, 
+                   dataset = "hsapiens_gene_ensembl")
+
+attributes <- c("ensembl_gene_id",
+                "hgnc_symbol",
+                "chromosome_name", 
+                "start_position", 
+                "end_position", 
+                "percentage_gene_gc_content",
+                "band",
+                "gene_biotype")
+
+annot <- getBM(mart = ensembl, attributes = attributes)
+
+
